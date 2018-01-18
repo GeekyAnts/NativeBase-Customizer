a41d6f5edaffe4799fe8a3ae0b963679bf1f403d
Supriya <>
2018-01-18 13:04:47 +0530

    Refactored structure as per NBKS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { AppRegistry } from 'react-native';
-import App from './js/App';
+import React from "react";
+import { render } from "react-dom";
+import { AppRegistry } from "react-native";
+import Setup from "./js/boot/setup";
 // render(<App />, document.getElementById('root'))
-AppRegistry.registerComponent('NativebaseKitchenSink', () => App);
-AppRegistry.runApplication('NativebaseKitchenSink', {
-  rootTag: document.getElementById('root')
+AppRegistry.registerComponent("NativebaseKitchenSink", () => Setup);
+AppRegistry.runApplication("NativebaseKitchenSink", {
+  rootTag: document.getElementById("root")
 });
